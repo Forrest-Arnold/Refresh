87bf9253442ff6fb9898f399c02a3042c20b8779
Forrest-Arnold <forkalarn@gmail.com>
2025-03-07 16:18:29 -0800

    Initial Commit

---
+++ Refresh/RefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefreshApp.swift
+//  Refresh
+//
+//  Created by Forrest Kalani Arnold on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RefreshTests/RefreshTests.swift
@@ -0,0 +1,16 @@
+//
+//  RefreshTests.swift
+//  RefreshTests
+//
+//  Created by Forrest Kalani Arnold on 3/7/25.
+//
+
+import Testing
+
+struct RefreshTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
